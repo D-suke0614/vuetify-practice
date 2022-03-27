3d6a21e48aa23416fedd560119da0ad5dbd16afb
Daisuke <d.suke.kida@gmail.com>
2022-03-27 14:36:56 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar class="bg-cyan-darken-1 text-white">
+      <v-app-bar-nav-icon variant="text" @click="drawer = !drawer"/>
+      <v-toolbar-title>Vuetify Practice</v-toolbar-title>
+      <v-space></v-space>
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+      <v-btn variant="text" icon="mdi-dots-vartical"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+
+    <v-footer class="bg-cyan-darken-1">
+      <v-row justify="center">
+        <v-btn v-for="item in items" :key="item" color="white" variant="text">
+          {{ item.title }}
+        </v-btn>
+        <v-col class="text-center text-white" cols="12">
+          {{ new Date().getFullYear() }} - <strong>Vuetify Practice</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -8,19 +35,19 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'ホーム', value: 'home' },
+      { title: '会社概要', value: 'companyr' },
+      { title: 'サービス', value: 'service' },
+      { title: 'お問い合わせ', value: 'inquiry' },
+    ]
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
